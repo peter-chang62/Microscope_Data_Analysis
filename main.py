59b2aefee29706d02febefd6d6f960f421ffa646
Peter Chang <pech6593@colorado.edu>
2022-09-12 17:39:30 -0600

    main.py

---
+++ main.py
@@ -0,0 +1,173 @@
+"""From having worked with scripting for a while now, I think I would actually like to have an interactive GUI for
+the purpose of figuring out the lock window. Specifically, I would like to be able to visualize where the spectrum
+would fall inside the nyquist window in both the optical domain, and in the DCS frequency domain, and be able to see
+where the f0's would fall """
+
+import scipy.constants as sc
+import PyQt5.QtWidgets as qt
+from Error_Window import Ui_Form
+import PlotWidgets as pw
+import PyQt5.QtGui as qtg
+from Gui_DCS_Lockpoints import Ui_MainWindow
+import pyqtgraph as pg
+
+
+class ErrorWindow(qt.QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+    def set_text(self, text):
+        self.textBrowser.setText(text)
+
+
+def raise_error(error_window, text):
+    error_window.set_text(text)
+    error_window.show()
+
+
+class Gui(qt.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.show()
+
+        self.error_window = ErrorWindow()
+
+        self.plot_window_optical = pw.PlotWindow(self.le_xmin,
+                                                 self.le_xmax,
+                                                 self.le_ymin,
+                                                 self.le_ymax,
+                                                 self.gv)
+        self.plot_window_rf = pw.PlotWindow(self.le_xmin_2,
+                                            self.le_xmax_2,
+                                            self.le_ymin_2,
+                                            self.le_ymax_2,
+                                            self.gv_2)
+        self.lr = pg.LinearRegionItem()
+        self.plot_window_optical.plotwidget.addItem(self.lr)
+        self.curve_optical = pw.create_curve()
+        self.curve_rf = pw.create_curve()
+        self.plot_window_optical.plotwidget.addItem(self.curve_optical)
+        self.plot_window_rf.plotwidget.addItem(self.curve_rf)
+
+        self.le_min_wl.setValidator(qtg.QDoubleValidator())
+        self.le_max_wl.setValidator(qtg.QDoubleValidator())
+        self.le_f01.setValidator(qtg.QDoubleValidator())
+        self.le_f02.setValidator(qtg.QDoubleValidator())
+        self.le_rep_rate.setValidator(qtg.QDoubleValidator())
+        self.le_dfrep.setValidator(qtg.QIntValidator())
+
+        self.lcd.setSegmentStyle(qt.QLCDNumber.Flat)
+
+        self.nu_min = sc.c / 5e-6
+        self.nu_max = sc.c / 3e-6
+        self.f01 = 240e6
+        self.f02 = 280e6
+        self.frep = 1000e6
+        self.dfrep = 50
+
+        self.verticalScrollBar.setMinimum(50)
+        self.verticalScrollBar.setMaximum(int(1e5))
+        self.verticalScrollBar.setValue(50)
+        self.verticalScrollBar.setPageStep(1)
+        self.verticalScrollBar.setSingleStep(1)
+        self.lcd.setDigitCount(6)
+        self.lcd.display(50)
+
+        self.lr.setRegion([self.nu_min * 1e-12, self.nu_max * 1e-12])
+
+        self.connect()
+
+    def connect(self):
+        self.le_min_wl.editingFinished.connect(self.update_wl_min)
+        self.le_max_wl.editingFinished.connect(self.update_wl_max)
+        self.le_f01.editingFinished.connect(self.update_f01)
+        self.le_f02.editingFinished.connect(self.update_f02)
+        self.le_rep_rate.editingFinished.connect(self.update_frep)
+        self.verticalScrollBar.valueChanged.connect(self.update_dfrep_from_lcd)
+        self.le_dfrep.editingFinished.connect(self.update_dfrep_from_le)
+
+    @property
+    def wl_min(self):
+        return sc.c / self.nu_max
+
+    @property
+    def wl_max(self):
+        return sc.c / self.nu_min
+
+    @wl_min.setter
+    def wl_min(self, val):
+        self.nu_max = sc.c / val
+        self.lr.setRegion([self.nu_min * 1e-12, self.nu_max * 1e-12])
+
+    @wl_max.setter
+    def wl_max(self, val):
+        self.nu_min = sc.c / val
+        self.lr.setRegion([self.nu_min * 1e-12, self.nu_max * 1e-12])
+
+    def update_wl_min(self):
+        wl_min = float(self.le_min_wl.text())
+        if wl_min <= 0:
+            raise_error(self.error_window, "min wavelength must be >0")
+            return
+        elif wl_min >= self.wl_max * 1e6:
+            raise_error(self.error_window, "min wavelength must be < max wavelength")
+            self.le_min_wl.setText(str(self.wl_min * 1e6))
+            return
+        self.wl_min = wl_min * 1e-6
+
+    def update_wl_max(self):
+        wl_max = float(self.le_max_wl.text())
+        if wl_max <= 0:
+            raise_error(self.error_window, "max wavelength must be >0")
+            return
+        elif wl_max <= self.wl_min * 1e6:
+            raise_error(self.error_window, "max wavelength must be > min wavelength")
+            self.le_max_wl.setText(str(self.wl_max * 1e6))
+            return
+        self.wl_max = wl_max * 1e-6
+
+    def update_f01(self):
+        f01 = float(self.le_f01.text())
+        f01 *= 1e6
+        if not (0 <= f01 <= self.frep):
+            raise_error(self.error_window, "f01 must be 0 <= f01 <= frep")
+            self.le_f01.setText(str(self.f01 * 1e-6))
+        self.f01 = f01
+
+    def update_f02(self):
+        f02 = float(self.le_f02.text())
+        f02 *= 1e6
+        if not (0 <= f02 <= self.frep):
+            raise_error(self.error_window, "f02 must be 0 <= f02 <= frep")
+            self.le_f02.setText(str(self.f02 * 1e-6))
+        self.f02 = f02
+
+    def update_frep(self):
+        frep = float(self.le_rep_rate.text())
+        frep *= 1e6
+        if any([frep < self.f01, frep < self.f02]):
+            raise_error(self.error_window, "frep must be >= f01 and f02")
+            self.le_rep_rate.setText(str(self.frep * 1e-6))
+            return
+        self.frep = frep
+
+    def update_dfrep_from_lcd(self):
+        self.lcd.display(self.verticalScrollBar.value())
+        self.dfrep = self.verticalScrollBar.value()
+
+    def update_dfrep_from_le(self):
+        dfrep = int(self.le_dfrep.text())
+        if not 50 <= dfrep <= 1e5:
+            raise_error(self.error_window, "delta frep must be 50 <= dfrep <= 100,000")
+            self.le_dfrep.setText(str(self.dfrep))
+            return
+        self.dfrep = dfrep
+        self.verticalScrollBar.setValue(self.dfrep)
+
+
+if __name__ == '__main__':
+    app = qt.QApplication([])
+    hey = Gui()
+    app.exec()
