76e18c962b94f0902ddb0e251ee71b9ff25d5627
Peter <pech6593@colorado.edu>
2022-11-11 12:34:55 -0700

    update

---
+++ plotting.py
@@ -0,0 +1,24 @@
+import numpy as np
+import clipboard_and_style_sheet as cr
+import matplotlib.pyplot as plt
+from scipy.integrate import simps
+from images import *
+
+cr.style_sheet()
+
+x_group3, y_group3, i_group3, s_group3 = group_3(False)
+x_num4, y_num4, i_num4, s_num4 = num_4(False)
+x_bar, y_bar, i_bar, s_bar = smallest_bar(False)
+
+ind_inbw_patt = [57, 35]
+ind_inbw_bar = [30, 27]
+ind_on_bar = [24, 19]
+
+snr_bckgnd = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
+                     r"snr_bckgnd.npy")
+snr_su8 = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
+                  r"snr_su8.npy")
+avg_bckgnd = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
+                     r"avg_bckgnd.npy")
+avg_su8 = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
+                  r"avg_su8.npy")
--- snr.py
+++ snr.py
@@ -16,47 +16,54 @@ su8 = np.load(path + "stage1_5300_stage2_8970_phase_corrected.npy", mmap_mode='r
 ppifg = len(bckgnd[0])
 center = ppifg // 2
 
-# ______________________________________ calculate background __________________________________________________________
-data = bckgnd
-avg = np.mean(data, 0)
-avg = avg - np.mean(avg)
-ft_avg = pc.fft(avg).__abs__()
 
-# low pass filtered background
-b_lp, a_lp = si.butter(4, .20, 'low')
-filtered = si.filtfilt(b_lp, a_lp, ft_avg)
+def calculate_snr(data, plot=False):
+    # ______________________________________ calculate background ______________________________________________________
+    avg = np.mean(data, 0)
+    avg = avg - np.mean(avg)
+    ft_avg = pc.fft(avg).__abs__()
 
-# area to calculate snr from
-freq = np.fft.fftshift(np.fft.fftfreq(len(data[0])))
-ll = np.argmin(abs(freq - .10784578053383662))
-ul = np.argmin(abs(freq - .19547047721757888))
-denom = filtered[ll:ul].copy()
+    # low pass filtered background
+    b_lp, a_lp = si.butter(4, .20, 'low')
+    filtered = si.filtfilt(b_lp, a_lp, ft_avg)
 
-# ______________________________________ calculate cumulative average __________________________________________________
-x = data[0]
-x = x - np.mean(x)
-ft = pc.fft(x).__abs__()
-num = ft[ll:ul]
-noise = np.std(num / denom)
+    # area to calculate snr from
+    freq = np.fft.fftshift(np.fft.fftfreq(len(data[0])))
+    ll = np.argmin(abs(freq - .10784578053383662))
+    ul = np.argmin(abs(freq - .19547047721757888))
+    denom = filtered[ll:ul].copy()
 
-NOISE = np.zeros(len(data))
-NOISE[0] = noise
-
-n = 1
-for dat in data[1:]:
-    x = x * n / (n + 1) + dat / (n + 1)
+    # ______________________________________ calculate cumulative average __________________________________________________
+    x = data[0]
     x = x - np.mean(x)
     ft = pc.fft(x).__abs__()
     num = ft[ll:ul]
     noise = np.std(num / denom)
-    NOISE[n] = noise
 
-    n += 1
-    print(len(data) - 1 - n)
+    NOISE = np.zeros(len(data))
+    NOISE[0] = noise
 
-dt = 1 / 13.47e3
-t = np.arange(0, len(NOISE) * dt, dt)
-fig, ax = plt.subplots(1, 1)
-ax.loglog(t, NOISE, 'o')
-ax.set_xlabel("time (s)")
-ax.set_ylabel("$\mathrm{\sigma}$")
+    n = 1
+    for dat in data[1:]:
+        x = x * n / (n + 1) + dat / (n + 1)
+        x = x - np.mean(x)
+        ft = pc.fft(x).__abs__()
+        num = ft[ll:ul]
+        noise = np.std(num / denom)
+        NOISE[n] = noise
+
+        n += 1
+        print(len(data) - 1 - n)
+
+    dt = 1 / 13.47e3
+    t = np.arange(0, len(NOISE) * dt, dt)
+    if plot:
+        fig, ax = plt.subplots(1, 1)
+        ax.loglog(t, NOISE, 'o')
+        ax.set_xlabel("time (s)")
+        ax.set_ylabel("$\mathrm{\sigma}$")
+
+    return np.c_[t, NOISE], avg
+
+# sigma_bckgnd, avg_bckgnd = calculate_snr(bckgnd)
+# sigma_su8, avg_su8 = calculate_snr(su8)
