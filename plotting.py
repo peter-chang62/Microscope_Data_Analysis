11159b2f3e66bccd22b600e7a7b7d95daae9182a
Peter <pech6593@colorado.edu>
2022-11-11 12:51:46 -0700

    update

--- plotting.py
+++ plotting.py
@@ -3,6 +3,10 @@ import clipboard_and_style_sheet as cr
 import matplotlib.pyplot as plt
 from scipy.integrate import simps
 from images import *
+import os
+
+if os.name == 'posix':
+    path = r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
 
 cr.style_sheet()
 
@@ -14,11 +18,7 @@ ind_inbw_patt = [57, 35]
 ind_inbw_bar = [30, 27]
 ind_on_bar = [24, 19]
 
-snr_bckgnd = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
-                     r"snr_bckgnd.npy")
-snr_su8 = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
-                  r"snr_su8.npy")
-avg_bckgnd = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
-                     r"avg_bckgnd.npy")
-avg_su8 = np.load(r"/home/peterchang/SynologyDrive/Research_Projects/Microscope/CLEO_2023/data_to_plot/"
-                  r"avg_su8.npy")
+snr_bckgnd = np.load(path + "snr_bckgnd.npy")
+snr_su8 = np.load(path + "snr_su8.npy")
+avg_bckgnd = np.load(path + "avg_bckgnd.npy")
+avg_su8 = np.load(path + "avg_su8.npy")
