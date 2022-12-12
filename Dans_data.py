76363e04488d4243204c46c1cd6bd83530d555fc
peter chang <pech6593@colorado.edu>
2022-12-12 14:33:00 -0700

    made td_phase_correct.py script independent of function definition file in preparation for generalizing to any data set

--- Dans_data.py
+++ Dans_data.py
@@ -1,15 +1,15 @@
+"""" optimiztion in the time domain doesn't work! It never gives an answer different from the initial guess,
+even when it's obviously at an unstable maximum. """
+
 import sys
 
 sys.path.append("include/")
 import numpy as np
-import matplotlib.pyplot as plt
-import clipboard_and_style_sheet
-import digital_phase_correction as dpc
 import phase_correction as pc
+import matplotlib.pyplot as plt
+import td_phase_correct as td
 
-clipboard_and_style_sheet.style_sheet()
-
-# ________________________________________________ path and load data __________________________________________________
+# ____________________________________ load the data ___________________________________________________________________
 path = r"data/Dans_interferograms/"
 data = np.genfromtxt(path + "Data.txt")
 T = np.genfromtxt(path + "t_axis.txt")
@@ -17,49 +17,18 @@ N = np.arange(-len(data[0]) // 2, len(data[0]) // 2)
 ppifg = len(N)
 center = ppifg // 2
 
-# ________________________________________________ apodization window __________________________________________________
-apod = 5000
+# ____________________________________ phase correction ________________________________________________________________
+opt = td.Optimize(data)
+opt.phase_correct(1500)
 
-# plt.plot(N, data[0])
-# plt.axvline(-apod // 2)
-# plt.axvline(apod // 2)
-
-# ________________________________________________ fitting phase, find frequency fit window ____________________________
-f, p, a, ax, ax2 = dpc.get_phase(data[np.random.randint(0, len(data))], apod)
-dpc.get_phase(data[np.random.randint(0, len(data))], apod, ax=ax, ax2=ax2)
-dpc.get_phase(data[np.random.randint(0, len(data))], apod, ax=ax, ax2=ax2)
-
-ll, ul = .200526, .202436
-# ll, ul = .20623, .20792
-
-plt.axvline(ll, color='r')
-plt.axvline(ul, color='r')
-
-# ________________________________________________ phase correction ____________________________________________________
-pdiff = dpc.get_pdiff(data, ll, ul, apod)
-dpc.apply_t0_and_phi0_shift(pdiff, data)
-
-# ________________________________________________ plot results  _______________________________________________________
-plt.figure()
-avg = np.mean(data, axis=0)
-ft = dpc.fft(avg)
-plt.plot(abs(ft))
+# __________________________________________ plotting __________________________________________________________________
+avg = np.mean(opt.CORR, axis=0)
+ft = abs(pc.fft(avg))
 
 plt.figure()
-[plt.plot(i[center - 150 // 2:center + 300 // 2]) for i in data]
-
-# ________________ same thing, but just do t0 correction via cross-correlation instead _________________________________
-data_ = np.genfromtxt(path + "Data.txt")
-data_ = (data.T - np.mean(data_, axis=1)).T
-data_, shift = pc.t0_correct_via_cross_corr(data_, 5000, False)
+plt.plot(ft[center:])
 
 plt.figure()
-avg_2 = np.mean(data_, axis=0)
-ft = dpc.fft(avg_2)
-plt.plot(abs(ft))
+plt.plot(avg)
 
-plt.figure()
-[plt.plot(i[center - 150 // 2:center + 300 // 2]) for i in data_]
-
-# for sublime-text
 plt.show()
---
+++ include/td_phase_correct.py
@@ -0,0 +1,79 @@
+"""" optimiztion in the time domain doesn't work! It never gives an answer different from the initial guess,
+even when it's obviously at an unstable maximum. """
+
+import numpy as np
+import scipy.signal as ss
+import scipy.optimize as so
+
+
+# shift a signal in time, I've vetted that this gives expected results
+def shift(x, dt):
+    ft = np.fft.fft(np.fft.ifftshift(x))
+    freq = np.fft.fftfreq(len(ft))
+    phase = np.exp(1j * 2 * np.pi * freq * dt)
+    ft *= phase
+    return np.fft.fftshift(np.fft.ifft(ft)).real
+
+
+# apply a phase offset, I've also vetted that this cycles the ceo phase by the expected amount
+def apply_phi0_shift(x, offst):
+    hbt = ss.hilbert(x)
+    hbt *= np.exp(1j * offst)
+    return hbt.real
+
+
+# function to minimize, I've vetted that this at least looks physical
+def error_dt_offst(X, x, x0, zoom):
+    dt, phi0 = X
+    ppifg = len(x)
+    center = ppifg // 2
+    y = shift(x, dt)  # shift x by dt
+    y = apply_phi0_shift(y, phi0)  # now apply phase offset to the shifted interferogram
+    return np.mean((x0[center - zoom // 2: center + zoom // 2] -
+                    y[center - zoom // 2: center + zoom // 2]) ** 2)
+
+
+# same as func but only with phase offset
+# I've vetted that this at least looks physical
+def error_offst(X, x, x0, zoom):
+    phi0 = X
+    ppifg = len(x)
+    center = ppifg // 2
+    y = apply_phi0_shift(x, phi0)  # apply phase offset to x
+    return np.mean((x0[center - zoom // 2: center + zoom // 2] -
+                    y[center - zoom // 2: center + zoom // 2]) ** 2)
+
+
+class Optimize:
+    def __init__(self, data):
+        self.data = data
+        self.data = (data.T / np.max(data, axis=1)).T
+        self.data = (data.T - np.mean(data, axis=1)).T
+
+    def error_shift_offst(self, X, n, zoom):
+        return error_dt_offst(X, self.data[n], self.data[0], zoom)
+
+    def error_offst(self, X, n, zoom):
+        return error_offst(X, self.data[n], self.data[0], zoom)
+
+    def phase_correct(self, zoom):
+        offst_with_dt_guess = np.zeros((len(self.data), 2))
+        CORR = self.data.copy()
+        ERROR = np.zeros((len(self.data)))
+        for n, i in enumerate(self.data):
+            # dt, phi0 = X
+            res = so.minimize(fun=self.error_shift_offst,
+                              x0=np.array([0, 0]),
+                              args=(n, zoom),
+                              method='Nelder-Mead')
+            offst_with_dt_guess[n] = res.x
+            ERROR[n] = res.fun
+
+            x = shift(CORR[n], res.x[0])
+            x = apply_phi0_shift(x, res.x[1])
+            CORR[n] = x
+
+            print(res.x)
+
+        self.CORR = CORR
+        self.ERROR = ERROR
