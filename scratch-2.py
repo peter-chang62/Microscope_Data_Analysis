ba7d7b32ffd56e7dc86e7c42f79a38cb35d9348c
peter chang <pech6593@colorado.edu>
2023-01-09 21:00:43 -0700

    update

--- scratch-2.py
+++ scratch-2.py
@@ -1,68 +1,101 @@
+"this is looking at the output of scratch-2.py"
+
 import sys
 
 sys.path.append("include/")
 import numpy as np
 import matplotlib.pyplot as plt
 import clipboard_and_style_sheet as cr
-import td_phase_correct as td
-import os
+import scipy.signal as si
 
 cr.style_sheet()
 
-
-def filt(freq, ll, ul, x, type="bp"):
-    if type == "bp":
-        return np.where(np.logical_and(freq > ll, freq < ul), x, 0)
-
-    elif type == "notch":
-        return np.where(np.logical_or(freq < ll, freq > ul), x, 0)
-
-
-if os.name == 'posix':
-    path = "/Users/peterchang/SynologyDrive/Research_Projects/" \
-           "Microscope/FreeRunningSpectra/11-09-2022/"
-else:
-    path = r"C:/Users/pchan/SynologyDrive/Research_Projects/Microscope" \
-           r"/FreeRunningSpectra/11-09-2022/"
-
-data = np.load(path + "stage1_5116_stage2_8500_53856x74180.npy",
+data = np.load("data/phase_corrected/"
+               "stage1_5116_stage2_8500_53856x74180_phase_corrected.npy",
                mmap_mode='r')
 
-ppifg = len(data[0])
-center = ppifg // 2
-N_IFG = len(data)
 
-data.resize((ppifg * N_IFG,))
-data = data[center:-center]
-data.resize((N_IFG - 1, ppifg))
-# lab frame frequency (0 to 500 MHz)
-freq = np.fft.ifftshift(np.fft.fftfreq(len(data[0]), 1e-9) * 1e-6)
-rfreq = np.fft.rfftfreq(len(data[0]), 1e-9) * 1e-6
+def calculate_snr(data, apod=None, plot=False):
+    ppifg = len(data[0])
+    center = ppifg // 2
+    if apod is not None:
+        data = data[:, center - apod // 2:center + apod // 2]
 
-# %% __________________________________________________________________________
-# filter the data first (using the 1 GHz resolution)
-data_filt = np.load("data/phase_corrected/" +
-                    "stage1_5116_stage2_8500_53856x74180_phase_corrected.npy",
-                    mmap_mode='r+')
-start = (len(data) // 4) * 3
-# end = (len(data) // 4) * 3
-end = len(data)
-h = 0
-for n in range(start, end):
-    ft = np.fft.rfft(data[n])  # make sure to load from data
-    w_filt = filt(rfreq, 0, 1, ft, "notch")
-    w_filt = filt(rfreq, 31.5, 32.5, w_filt, "notch")
-    w_filt = filt(rfreq, 249.5, 250.5, w_filt, "notch")
-    w_filt = filt(rfreq, 281, 283, w_filt, "notch")
-    w_filt = filt(rfreq, 336, 339.5, w_filt, "notch")
-    w_filt = filt(rfreq, 499, 500, w_filt, "notch")
+    avg = np.mean(data, 0)
+    avg -= np.mean(avg)
 
-    t_filt = np.fft.irfft(w_filt)
-    data_filt[n] = t_filt
-    print(end - start - h - 1)
-    h += 1
+    b, a = si.butter(4, .20, 'low')
+    filt = si.filtfilt(b, a, np.fft.rfft(avg).__abs__())
+    freq = np.fft.rfftfreq(len(avg))
+    ll = np.argmin(abs(freq - .10784578053383662))
+    ul = np.argmin(abs(freq - .19547047721757888))
+    denom = filt[ll:ul]
 
-# %% __________________________________________________________________________
-# now phase correct the filtered data (1 GHz resolution)
-opt = td.Optimize(data_filt[:, center - 10:center + 10])
-opt.phase_correct(data_filt, start_index=start, end_index=end)
+    x = data[0]
+    x = x - np.mean(x)
+    ft = np.fft.rfft(x).__abs__()
+    num = ft[ll:ul]
+    absorption = num / denom
+    absorbance = -np.log(absorption)
+    noise = np.std(absorbance)
+
+    NOISE = np.zeros(len(data))
+    NOISE[0] = noise
+
+    n = 1
+    for dat in data[1:]:
+        x = (x * n + dat) / (n + 1)
+        x -= np.mean(x)
+        ft = np.fft.rfft(x).__abs__()
+        num = ft[ll:ul]
+        absorption = num / denom
+        absorbance = -np.log(absorption)
+        noise = np.std(absorbance)
+        NOISE[n] = noise
+        n += 1
+        print(len(data) - 1 - n)
+
+    dt = ppifg / 1e9
+    t = np.arange(0, len(NOISE) * dt, dt)
+
+    if plot:
+        plt.figure()
+        plt.loglog(t, NOISE, 'o')
+        plt.xlabel("time (s)")
+        plt.ylabel("$\\mathrm{\\sigma}$")
+
+    class result:
+        def __init__(self):
+            self.sigma = np.c_[t, NOISE]
+            self.avg = avg
+
+    return result()
+
+
+full = calculate_snr(data, apod=None, plot=False)
+_1e4 = calculate_snr(data, apod=int(1e4), plot=False)
+_1e3 = calculate_snr(data, apod=int(1e3), plot=False)
+
+# %% save
+np.save("data/phase_corrected/full.npy", full.sigma)
+np.save("data/phase_corrected/1e4_pts.npy", _1e4.sigma)
+np.save("data/phase_corrected/1e3.npy", _1e3.sigma)
+
+# %% plot
+# plt.figure()
+# plt.loglog(full.sigma[:, 0], full.sigma[:, 1], 'o', label="full interferogram")
+# plt.plot(_1e4.sigma[:, 0], _1e4.sigma[:, 1], 'o', label="10,000 pt window")
+# plt.plot(_1e3.sigma[:, 0], _1e3.sigma[:, 1], 'o', label="1000 pt window")
+# plt.xlabel("time (s)")
+# plt.ylabel("absorbance noise (a.u.)")
+# plt.legend(loc='best')
+#
+# plt.figure()
+# plt.plot(np.fft.rfftfreq(len(full.avg)),
+#          np.fft.rfft(full.avg).__abs__(), label="full interferogram")
+# plt.plot(np.fft.rfftfreq(len(_1e4.avg)),
+#          np.fft.rfft(_1e4.avg).__abs__(), label="10,000 pt window")
+# plt.plot(np.fft.rfftfreq(len(_1e3.avg)),
+#          np.fft.rfft(_1e3.avg).__abs__(), label="1000 pt window")
+# plt.legend(loc='best')
+# plt.xlabel("MHz")
--- scratch.py
+++ scratch.py
@@ -10,14 +10,6 @@ import os
 cr.style_sheet()
 
 
-def rfft(x):
-    return np.fft.rfft(x)
-
-
-def irfft(x):
-    return np.fft.irfft(x)
-
-
 def filt(freq, ll, ul, x, type="bp"):
     if type == "bp":
         return np.where(np.logical_and(freq > ll, freq < ul), x, 0)
@@ -33,8 +25,9 @@ else:
     path = r"C:/Users/pchan/SynologyDrive/Research_Projects/Microscope" \
            r"/FreeRunningSpectra/11-09-2022/"
 
-data = np.load(path + "stage1_5116_stage2_8500_53856x74180.npy",
-               mmap_mode='r')
+data = np.load(
+    path + "stage1_5300_stage2_8970_53856x74180.npy",
+    mmap_mode='r')
 
 ppifg = len(data[0])
 center = ppifg // 2
@@ -43,57 +36,21 @@ N_IFG = len(data)
 data.resize((ppifg * N_IFG,))
 data = data[center:-center]
 data.resize((N_IFG - 1, ppifg))
-data = data[::50]
+# lab frame frequency (0 to 500 MHz)
 freq = np.fft.ifftshift(np.fft.fftfreq(len(data[0]), 1e-9) * 1e-6)
-
-# %% __________________________________________________________________________
-# straight up regular phase correction (time domain)
-opt = td.Optimize(data[:, center - 10:center + 10])
-corr = data.copy()
-opt.phase_correct(corr)
-
-# %% __________________________________________________________________________
-# phase correct interferograms to overlap the f0 oscillations
-# subst = data[:, :1000]
-# opt = td.Optimize(subst)
-# corr = data.copy()
-# opt.phase_correct(corr)
-#
-# # %% look at the difference for the averages (I know it's wrong because
-# # you didn't overlap the centerbursts), for when the centerburst is removed
-# # and when it's left in. It looks weird
-# avg = np.mean(corr, 0)
-# bckgnd = np.hstack([avg[:center - 5], avg[center + 5:]])
-# plt.plot(np.fft.fftfreq(len(bckgnd), 1e-9) * 1e-6,
-#          dpc.fft(bckgnd).__abs__())
-# plt.plot(freq, dpc.fft(avg).__abs__())
-# plt.ylim(0, 1.2e4)
-
-
-# %% __________________________________________________________________________
-# x = data[0].copy()
-# ft = rfft(x)
-# rfreq = np.fft.rfftfreq(len(x), 1e-9) * 1e-6
-# w_filt = filt(rfreq, 270, 295, ft, "notch")
-# t_filt = np.fft.irfft(w_filt)
-#
-# # %%
-# plt.figure()
-# plt.plot(freq, dpc.fft(x).__abs__())
-# plt.plot(rfreq, ft.__abs__())
-# plt.plot(rfreq, w_filt.__abs__())
-#
-# # %%
-# plt.figure()
-# plt.plot(x)
-# plt.plot(t_filt)
+rfreq = np.fft.rfftfreq(len(data[0]), 1e-9) * 1e-6
 
 # %% __________________________________________________________________________
 # filter the data first (using the 1 GHz resolution)
-data_filt = np.asarray(data).copy()  # temporary solution (looking at a subset)
-rfreq = np.fft.rfftfreq(len(data_filt[0]), 1e-9) * 1e-6
-for n, x in enumerate(data_filt):
-    ft = rfft(x)
+data_filt = np.load("data/phase_corrected/" +
+                    "stage1_5300_stage2_8970_53856x74180_phase_corrected.npy",
+                    mmap_mode='r+')
+start = (len(data) // 4) * 3
+# end = (len(data) // 4) * 3
+end = len(data)
+h = 0
+for n in range(start, end):
+    ft = np.fft.rfft(data[n])  # make sure to load from data
     w_filt = filt(rfreq, 0, 1, ft, "notch")
     w_filt = filt(rfreq, 31.5, 32.5, w_filt, "notch")
     w_filt = filt(rfreq, 249.5, 250.5, w_filt, "notch")
@@ -103,32 +60,10 @@ for n, x in enumerate(data_filt):
 
     t_filt = np.fft.irfft(w_filt)
     data_filt[n] = t_filt
-    print(len(data_filt) - n - 1)
+    print(end - start - h - 1)
+    h += 1
 
-# %%
-opt = td.Optimize(data_filt[:, center - 10:center + 10])
-opt.phase_correct(data_filt)
-
-avg = np.mean(data_filt, 0)
-ft_avg = np.fft.rfft(avg)
-ft_apd = np.fft.rfft(avg[center - 1000:center + 1000])
-rfreq_apd = np.fft.rfftfreq(len(avg[center - 1000:center + 1000]), 1e-9) * 1e-6
-
-# %%
-fig, ax = plt.subplots(1, 2, figsize=np.array([11.19, 4.8]))
-ax[0].plot(rfreq, ft_avg.__abs__())
-ax[0].plot(rfreq_apd, ft_apd.__abs__(), label="apodized")
-ax[0].legend(loc='best')
-ax[0].set_ylim(0, 2.9e4)
-ax[0].set_title("unfiltered")
-
-ax[1].plot(rfreq, np.fft.rfft(np.mean(corr, 0)).__abs__())
-ax[1].plot(
-    np.fft.rfftfreq(len(corr[0][center - 1000:center + 1000]), 1e-9) * 1e-6,
-    np.fft.rfft(np.mean(corr[:, center - 1000:center + 1000], 0)).__abs__(),
-    label="apodized")
-ax[1].legend(loc='best')
-ax[1].set_ylim(0, 2.9e4)
-ax[1].set_title("filtered")
-
-plt.show()
+# %% __________________________________________________________________________
+# now phase correct the filtered data (1 GHz resolution)
+opt = td.Optimize(data_filt[:, center - 50:center + 50])
+opt.phase_correct(data_filt, start_index=start, end_index=end)
