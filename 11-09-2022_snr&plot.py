578bd89ed62bedf31e1e48cc0f9c7f3c8eaf9d1b
peter chang <pech6593@colorado.edu>
2023-01-25 14:46:34 -0700

    tqdm looks pretty cool when integrated in for loops

--- 11-09-2022_snr&plot.py
+++ 11-09-2022_snr&plot.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import clipboard_and_style_sheet as cr
 import scipy.signal as si
+from tqdm import tqdm
 from numpy import ma
 
 cr.style_sheet()
@@ -58,7 +59,7 @@ def calculate_snr(data, apod=None):
 
     x = 0
     NOISE = np.zeros(len(data))
-    for n, i in enumerate(data):
+    for n, i in enumerate(tqdm(data)):
         i = i - np.mean(i)
 
         ft = np.fft.rfft(i)
@@ -70,97 +71,95 @@ def calculate_snr(data, apod=None):
         noise = np.std(absorbance)
         NOISE[n] = noise
 
-        print(len(data) - n - 1)
-
     return NOISE
 
 
 # %% __________________________________________________________________________
-# # data = np.load( # taken on silicon background
-# #     "data/phase_corrected/stage1_5116_stage2_8500_53856x74180_phase_corrected.npy",
-# #     mmap_mode='r')
-# data = np.load(  # taken on su8
-#     "data/phase_corrected/stage1_5300_stage2_8970_53856x74180_phase_corrected.npy",
+# data = np.load( # taken on silicon background
+#     "data/phase_corrected/stage1_5116_stage2_8500_53856x74180_phase_corrected.npy",
 #     mmap_mode='r')
-# ppifg = len(data[0])
-# center = ppifg // 2
-#
-# resolution = np.arange(0, 500 + 10, 10)
-# resolution[0] = 1
-# APOD = (1 / resolution) * ppifg
-# APOD = np.round(APOD).astype(int)
-# APOD = np.where(APOD % 2 == 0, APOD, APOD + 1)
-#
-# APOD = ma.asarray(APOD)
-# APOD[0] = ma.masked
-#
-# SIGMA = np.zeros((len(APOD), len(data)))
-# for n, apod in enumerate(APOD):
-#     SIGMA[n] = calculate_snr(data, apod)
-#     print(f'_____________________{len(APOD) - n - 1}_____________________')
-#
-# np.save("data/phase_corrected/su8/sigma/sigma.npy", SIGMA)
-
-# %% __________________________________________________________________________
-sigma_su8 = np.load("data/phase_corrected/su8/sigma/sigma.npy")
-sigma_bckgnd = np.load("data/phase_corrected/bckgnd/sigma/sigma.npy")
-window = np.load("data/phase_corrected/su8/sigma/NPTS.npy")
-ppifg = 74180
+data = np.load(  # taken on su8
+    "data/phase_corrected/stage1_5300_stage2_8970_53856x74180_phase_corrected.npy",
+    mmap_mode='r')
+ppifg = len(data[0])
 center = ppifg // 2
 
-n_ifg = np.arange(1, len(sigma_bckgnd[0]) + 1)
-s_bckgnd_dB = 10 * np.log10(sigma_bckgnd)
-s_su8_dB = 10 * np.log10(sigma_su8)
+resolution = np.arange(0, 500 + 10, 10)
+resolution[0] = 1
+APOD = (1 / resolution) * ppifg
+APOD = np.round(APOD).astype(int)
+APOD = np.where(APOD % 2 == 0, APOD, APOD + 1)
 
-resolution = window[0] / window
-resolution = np.round(resolution, 0)
+APOD = ma.asarray(APOD)
+APOD[0] = ma.masked
 
-fig = plt.figure()
-plt.suptitle("background absorbance noise (dB)")
-plt.pcolormesh(n_ifg, resolution, s_bckgnd_dB, cmap='jet')
-plt.xscale('log')
-plt.xlabel("# interferograms")
-plt.ylabel("resolution (GHz)")
-plt.colorbar()
+SIGMA = np.zeros((len(APOD), len(data)))
+for n, apod in enumerate(APOD):
+    SIGMA[n] = calculate_snr(data, apod)
+    print(f'_____________________{len(APOD) - n - 1}_____________________')
 
-fig = plt.figure()
-plt.suptitle("su8 absorbance noise (dB)")
-plt.pcolormesh(n_ifg, resolution, s_su8_dB, cmap='jet')
-plt.xscale('log')
-plt.xlabel("# interferograms")
-plt.ylabel("resolution (GHz)")
-plt.colorbar()
+np.save("data/phase_corrected/su8/sigma/sigma.npy", SIGMA)
 
-# # create a gif showing how the absorbance noise changes with
-# # apodization window
-# fig, ax = plt.subplots(1, 2, figsize=np.array([10.64, 4.8]))
-# avg = np.load("data/phase_corrected/bckgnd/avg_bckgnd.npy")
-# freq_full = np.fft.rfftfreq(len(avg))
-# s_full = apply_filter(np.fft.rfft(avg).__abs__())
-# ind_full = np.logical_and(freq_full > 0.10784578053383662,
-#                           freq_full < 0.19547047721757888).nonzero()[0]
-# save = True
-# for h, w in enumerate(window):
-#     x = avg[center - w // 2:center + w // 2]
-#     s = apply_filter(np.fft.rfft(x).__abs__())
-#     freq = np.fft.rfftfreq(len(x))
-#     ind = np.logical_and(freq > 0.10784578053383662,
-#                          freq < 0.19547047721757888).nonzero()[0]
+# %% __________________________________________________________________________
+# sigma_su8 = np.load("data/phase_corrected/su8/sigma/sigma.npy")
+# sigma_bckgnd = np.load("data/phase_corrected/bckgnd/sigma/sigma.npy")
+# window = np.load("data/phase_corrected/su8/sigma/NPTS.npy")
+# ppifg = 74180
+# center = ppifg // 2
 #
-#     [i.clear() for i in ax]
-#     ax[0].plot(freq_full[ind_full], s_full[ind_full])
-#     ax[0].plot(freq[ind], s[ind])
-#     ax[1].loglog(sigma_bckgnd[0], 'o', label="1 GHz")
-#     ax[1].loglog(sigma_bckgnd[h], 'o',
-#                  label=f'{np.round(74180 / window[h], 1)} GHz')
-#     ax[1].set_ylim(
-#         0.0005,  # bckgnd
-#         # 0.0017, # su8
-#         0.8)
-#     ax[1].legend(loc='best')
-#     if save:
-#         plt.savefig(f'fig/{h}.png')
-#     else:
-#         plt.pause(.05)
+# n_ifg = np.arange(1, len(sigma_bckgnd[0]) + 1)
+# s_bckgnd_dB = 10 * np.log10(sigma_bckgnd)
+# s_su8_dB = 10 * np.log10(sigma_su8)
 #
-#     print(len(window) - h - 1)
+# resolution = window[0] / window
+# resolution = np.round(resolution, 0)
+#
+# fig = plt.figure()
+# plt.suptitle("background absorbance noise (dB)")
+# plt.pcolormesh(n_ifg, resolution, s_bckgnd_dB, cmap='jet')
+# plt.xscale('log')
+# plt.xlabel("# interferograms")
+# plt.ylabel("resolution (GHz)")
+# plt.colorbar()
+#
+# fig = plt.figure()
+# plt.suptitle("su8 absorbance noise (dB)")
+# plt.pcolormesh(n_ifg, resolution, s_su8_dB, cmap='jet')
+# plt.xscale('log')
+# plt.xlabel("# interferograms")
+# plt.ylabel("resolution (GHz)")
+# plt.colorbar()
+#
+# # # create a gif showing how the absorbance noise changes with
+# # # apodization window
+# # fig, ax = plt.subplots(1, 2, figsize=np.array([10.64, 4.8]))
+# # avg = np.load("data/phase_corrected/bckgnd/avg_bckgnd.npy")
+# # freq_full = np.fft.rfftfreq(len(avg))
+# # s_full = apply_filter(np.fft.rfft(avg).__abs__())
+# # ind_full = np.logical_and(freq_full > 0.10784578053383662,
+# #                           freq_full < 0.19547047721757888).nonzero()[0]
+# # save = True
+# # for h, w in enumerate(window):
+# #     x = avg[center - w // 2:center + w // 2]
+# #     s = apply_filter(np.fft.rfft(x).__abs__())
+# #     freq = np.fft.rfftfreq(len(x))
+# #     ind = np.logical_and(freq > 0.10784578053383662,
+# #                          freq < 0.19547047721757888).nonzero()[0]
+# #
+# #     [i.clear() for i in ax]
+# #     ax[0].plot(freq_full[ind_full], s_full[ind_full])
+# #     ax[0].plot(freq[ind], s[ind])
+# #     ax[1].loglog(sigma_bckgnd[0], 'o', label="1 GHz")
+# #     ax[1].loglog(sigma_bckgnd[h], 'o',
+# #                  label=f'{np.round(74180 / window[h], 1)} GHz')
+# #     ax[1].set_ylim(
+# #         0.0005,  # bckgnd
+# #         # 0.0017, # su8
+# #         0.8)
+# #     ax[1].legend(loc='best')
+# #     if save:
+# #         plt.savefig(f'fig/{h}.png')
+# #     else:
+# #         plt.pause(.05)
+# #
+# #     print(len(window) - h - 1)
