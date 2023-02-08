117a6e4b4f2df3234b64982a4048bb044e4d7abf
peter chang <pech6593@colorado.edu>
2023-02-07 22:48:11 -0700

    formatting updates and spyder project

--- 11-09-2022.py
+++ 11-09-2022.py
@@ -9,7 +9,9 @@ import clipboard_and_style_sheet
 
 path = r"D:\Microscope\11-09-2022/"
 # data = np.load(path + "stage1_5116_stage2_8500_53856x74180.npy") # bckgnd
-data = np.load(path + "stage1_5300_stage2_8970_53856x74180.npy")  # absorption on SU-8 bar
+data = np.load(
+    path + "stage1_5300_stage2_8970_53856x74180.npy"
+)  # absorption on SU-8 bar
 N_ifgs = len(data) - 1
 ppifg = len(data[0])
 center = ppifg // 2
@@ -21,13 +23,13 @@ data.resize((N_ifgs, ppifg))
 mean = np.mean(data, axis=1)
 data = (data.T - mean).T
 
-ll_freq = .1155
+ll_freq = 0.1155
 ul_freq = 0.17
 pdiff = dpc.get_pdiff(data, ll_freq, ul_freq, 200)
 
 h = 0
 step = 250
 while h < len(data):
-    dpc.apply_t0_and_phi0_shift(pdiff[h:h + step], data[h:h + step])
+    dpc.apply_t0_and_phi0_shift(pdiff[h : h + step], data[h : h + step])
     h += step
     print(len(data) - h)
--- 11-09-2022_snr&plot.py
+++ 11-09-2022_snr&plot.py
@@ -10,13 +10,17 @@ from scipy.interpolate import InterpolatedUnivariateSpline
 cr.style_sheet()
 
 # together
-list_filter = np.array([[0, 2],
-                        [31, 32.8],
-                        [249.5, 250.5],
-                        [280, 284],
-                        [337, 338],
-                        [436, 437],
-                        [499, 500]])
+list_filter = np.array(
+    [
+        [0, 2],
+        [31, 32.8],
+        [249.5, 250.5],
+        [280, 284],
+        [337, 338],
+        [436, 437],
+        [499, 500],
+    ]
+)
 
 
 def filt(freq, ll, ul, x, type="bp"):
@@ -49,19 +53,19 @@ def calculate_snr(data, apod=None, avg_f=None):
 
     if not np.any([apod is None, apod == np.nan, ma.is_masked(apod)]):
         print("apodizing data")
-        data = data[:, center - apod // 2:center + apod // 2]
+        data = data[:, center - apod // 2 : center + apod // 2]
     else:
         print("NOT apodizing data")
     freq_a = np.fft.rfftfreq(len(data[0]))  # apodized frequency axis
 
-    b, a = si.butter(4, .2, "low")
+    b, a = si.butter(4, 0.2, "low")
     amp_ft_f_filt = si.filtfilt(b, a, abs(ft_f))  # filter 1 GHz spectrum
     # interpolate 1 GHz spectrum onto the coarser frequency axis
     amp_ft_f_filt_gridded = InterpolatedUnivariateSpline(freq_f, amp_ft_f_filt)
     amp_ft_f_filt_interp = amp_ft_f_filt_gridded(freq_a)
     # look at signal range of interest
-    ll_a = np.argmin(abs(freq_a - .10784578053383662))
-    ul_a = np.argmin(abs(freq_a - .19547047721757888))
+    ll_a = np.argmin(abs(freq_a - 0.10784578053383662))
+    ul_a = np.argmin(abs(freq_a - 0.19547047721757888))
     denom_f = amp_ft_f_filt_interp[ll_a:ul_a]
 
     x = 0
@@ -81,11 +85,13 @@ def calculate_snr(data, apod=None, avg_f=None):
     return NOISE
 
 
-# %% __________________________________________________________________________
-path = r"/Volumes/Extreme SSD/Research_Projects/Microscope/Python_Workspace" \
-       r"/data/phase_corrected/"
+# %%
+path = (
+    r"/Volumes/Extreme SSD/Research_Projects/Microscope/Python_Workspace"
+    r"/data/phase_corrected/"
+)
 
-# %% __________________________________________________________________________
+# %%
 # # data = np.load(  # taken on silicon
 # #     path + "stage1_5116_stage2_8500_53856x74180_phase_corrected.npy",
 # #     mmap_mode='r')
@@ -93,35 +99,42 @@ path = r"/Volumes/Extreme SSD/Research_Projects/Microscope/Python_Workspace" \
 # #               "/Python_Workspace/data/phase_corrected/bckgnd/avg_bckgnd.npy")
 # data = np.load(  # taken on su8
 #     path + "stage1_5300_stage2_8970_53856x74180_phase_corrected.npy",
-#     mmap_mode='r')
-# avg = np.load("/Volumes/Extreme SSD/Research_Projects/Microscope"
-#               "/Python_Workspace/data/phase_corrected/su8/avg_su8.npy")
+#     mmap_mode="r",
+# )
+# avg = np.load(
+#     "/Volumes/Extreme SSD/Research_Projects/Microscope"
+#     "/Python_Workspace/data/phase_corrected/su8/avg_su8.npy"
+# )
 # ppifg = len(data[0])
 # center = ppifg // 2
-#
+
 # resolution = np.arange(0, 500 + 10, 10)
 # resolution[0] = 1
 # APOD = (1 / resolution) * ppifg
 # APOD = np.round(APOD).astype(int)
 # APOD = np.where(APOD % 2 == 0, APOD, APOD + 1)
-#
+
 # APOD = ma.asarray(APOD)
 # APOD[0] = ma.masked
-#
+
 # SIGMA = np.zeros((len(APOD), len(data)))
 # for n, apod in enumerate(APOD):
 #     SIGMA[n] = calculate_snr(data, apod, avg)
-#     print(f'_____________________{len(APOD) - n - 1}_____________________')
-#
+#     print(f"_____________________{len(APOD) - n - 1}_____________________")
+
 # np.save("sigma_su8.npy", SIGMA)
 
-# %% __________________________________________________________________________
-s_su8 = np.load("/Volumes/Extreme SSD/Research_Projects/Microscope"
-                "/Python_Workspace/data/phase_corrected/su8/sigma/sigma"
-                ".npy")
-s_bckgnd = np.load("/Volumes/Extreme SSD/Research_Projects/Microscope"
-                   "/Python_Workspace/data/phase_corrected/bckgnd/sigma/sigma"
-                   ".npy")
+# %%
+s_su8 = np.load(
+    "/Volumes/Extreme SSD/Research_Projects/Microscope"
+    "/Python_Workspace/data/phase_corrected/su8/sigma/sigma"
+    ".npy"
+)
+s_bckgnd = np.load(
+    "/Volumes/Extreme SSD/Research_Projects/Microscope"
+    "/Python_Workspace/data/phase_corrected/bckgnd/sigma/sigma"
+    ".npy"
+)
 window = np.load(path + "su8/sigma/NPTS.npy")
 ppifg = 74180
 center = ppifg // 2
@@ -137,60 +150,68 @@ resolution = window[0] / window
 resolution = np.round(resolution, 0)
 
 # snr 2D plots
-# fig = plt.figure()
-# plt.suptitle("background absorbance noise (dB)")
-# plt.pcolormesh(n_ifg, resolution, snr_bckgnd_dB, cmap='jet')
-# plt.xscale('log')
-# plt.xlabel("# interferograms")
-# plt.ylabel("resolution (GHz)")
-# plt.colorbar()
-#
-# fig = plt.figure()
-# plt.suptitle("su8 absorbance noise (dB)")
-# plt.pcolormesh(n_ifg, resolution, snr_su8_dB, cmap='jet')
-# plt.xscale('log')
-# plt.xlabel("# interferograms")
-# plt.ylabel("resolution (GHz)")
-# plt.colorbar()
+fig = plt.figure()
+plt.suptitle("background absorbance noise (dB)")
+plt.pcolormesh(n_ifg, resolution, snr_bckgnd_dB, cmap="jet")
+plt.xscale("log")
+plt.xlabel("# interferograms")
+plt.ylabel("resolution (GHz)")
+plt.colorbar()
 
-# %% __________________________________________________________________________
+fig = plt.figure()
+plt.suptitle("su8 absorbance noise (dB)")
+plt.pcolormesh(n_ifg, resolution, snr_su8_dB, cmap="jet")
+plt.xscale("log")
+plt.xlabel("# interferograms")
+plt.ylabel("resolution (GHz)")
+plt.colorbar()
+
+# %%
 # create a gif
 target = "su8"
-save = True
+save = False
 
 if target == "su8":
-    avg = np.load("/Volumes/Extreme SSD/Research_Projects/Microscope"
-                  "/Python_Workspace/data/phase_corrected/su8/avg_su8.npy")
-    data = np.load("/Volumes/Extreme SSD/Research_Projects/Microscope"
-                   "/Python_Workspace/data/phase_corrected"
-                   "/stage1_5300_stage2_8970_53856x74180_phase_corrected.npy",
-                   mmap_mode='r')
+    avg = np.load(
+        "/Volumes/Extreme SSD/Research_Projects/Microscope"
+        "/Python_Workspace/data/phase_corrected/su8/avg_su8.npy"
+    )
+    data = np.load(
+        "/Volumes/Extreme SSD/Research_Projects/Microscope"
+        "/Python_Workspace/data/phase_corrected"
+        "/stage1_5300_stage2_8970_53856x74180_phase_corrected.npy",
+        mmap_mode="r",
+    )
     snr = snr_su8_dB
 elif target == "bckgnd":
-    avg = np.load("/Volumes/Extreme SSD/Research_Projects/Microscope"
-                  "/Python_Workspace/data/phase_corrected/bckgnd/avg_bckgnd"
-                  ".npy")
-    data = np.load("/Volumes/Extreme SSD/Research_Projects/Microscope"
-                   "/Python_Workspace/data/phase_corrected"
-                   "/stage1_5116_stage2_8500_53856x74180_phase_corrected.npy",
-                   mmap_mode='r')
+    avg = np.load(
+        "/Volumes/Extreme SSD/Research_Projects/Microscope"
+        "/Python_Workspace/data/phase_corrected/bckgnd/avg_bckgnd"
+        ".npy"
+    )
+    data = np.load(
+        "/Volumes/Extreme SSD/Research_Projects/Microscope"
+        "/Python_Workspace/data/phase_corrected"
+        "/stage1_5116_stage2_8500_53856x74180_phase_corrected.npy",
+        mmap_mode="r",
+    )
     snr = snr_bckgnd_dB
 avg -= np.mean(avg)
 avg_1000 = np.mean(data[:1000], axis=0)
 
 ft_f = np.fft.rfft(avg)
 f_f = np.fft.rfftfreq(avg.size, d=1e-9) * ppifg
-f_ll, f_ul = .10784578053383662, .19547047721757888
+f_ll, f_ul = 0.10784578053383662, 0.19547047721757888
 f_ll = f_ll * ppifg * 1e9 + f_f[-1] * 2
 f_ul = f_ul * ppifg * 1e9 + f_f[-1] * 2
 f_f += f_f[-1] * 2  # 3rd Nyquist window
-b, a = si.butter(4, .2, "low")
+b, a = si.butter(4, 0.2, "low")
 amp_ft_f_filt = si.filtfilt(b, a, abs(ft_f))  # filter 1 GHz spectrum
 
 fig, ax = plt.subplots(1, 2, figsize=np.array([10.98, 4.8]))
 for n in range(resolution.size):
     npts = window[n]
-    avg_a = avg_1000[center - npts // 2:center + npts // 2]
+    avg_a = avg_1000[center - npts // 2 : center + npts // 2]
 
     ft_a = np.fft.rfft(avg_a)
     f_a = np.fft.rfftfreq(avg_a.size, d=1e-9) * ppifg
@@ -202,20 +223,20 @@ for n in range(resolution.size):
     [i.clear() for i in ax]
     ax[0].plot(wl_a * 1e6, amp_ft_f_filt_interp, label="background")
     ax[0].plot(wl_a * 1e6, abs(ft_a), label="signal")
-    ax[0].axvline(sc.c * 1e6 / f_ll, color='r', linestyle='--')
-    ax[0].axvline(sc.c * 1e6 / f_ul, color='r', linestyle='--')
-    ax[1].semilogx(n_ifg, snr[0], 'o')
-    ax[1].semilogx(n_ifg, snr[n], 'o')
+    ax[0].axvline(sc.c * 1e6 / f_ll, color="r", linestyle="--")
+    ax[0].axvline(sc.c * 1e6 / f_ul, color="r", linestyle="--")
+    ax[1].semilogx(n_ifg, snr[0], "o")
+    ax[1].semilogx(n_ifg, snr[n], "o")
 
-    ax[0].legend(loc='best')
+    ax[0].legend(loc="best")
     ax[0].set_xlabel("wavelength ($\\mathrm{\\mu m}$)")
     ax[1].set_xlabel("interferogram #")
     ax[0].set_ylabel("power spectrum (a.u.)")
     ax[1].set_ylabel("SNR (dB)")
-    ax[1].axvline(1000, color='r', linestyle='--')
-    fig.suptitle(f'{int(resolution[n])} GHz resolution')
+    ax[1].axvline(1000, color="r", linestyle="--")
+    fig.suptitle(f"{int(resolution[n])} GHz resolution")
 
     if save:
-        plt.savefig(f'fig/{n}.png')
+        plt.savefig(f"fig/{n}.png")
     else:
-        plt.pause(.1)
+        plt.pause(0.1)
